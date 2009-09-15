44481bec49fa8e4ed9cadd274a0f09f87e849ef8
trigger-happy <triggerhappyelite@gmail.com>
2009-09-15 21:59:29 +0800

    Added support for unit testing
    Demonstrated how to do unit tests

---
+++ src/test/tester.cpp
@@ -0,0 +1,13 @@
+#include "tester.h"
+
+void Tester::tcp_test()
+{
+	m_socket = new QTcpSocket(this);
+	QVERIFY(!m_socket->isOpen());
+	m_socket->connectToHost(QString("localhost"), 21);
+	m_socket->waitForConnected();
+	QVERIFY(m_socket->isOpen());
+	delete m_socket;
+}
+
+QTEST_MAIN(Tester)
---
+++ src/test/tester.h
@@ -0,0 +1,15 @@
+#ifndef TESTER_H
+#define TESTER_H
+#include <QtTest/QTest>
+#include <QtNetwork>
+#include <QObject>
+
+class Tester : public QObject{
+	Q_OBJECT;
+	private slots:
+		void tcp_test();
+	private:
+		QTcpSocket* m_socket;
+};
+
+#endif //TESTER_H
