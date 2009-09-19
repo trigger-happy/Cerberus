d4b27198fc86a6136bf7031871356c621d9604fb
trigger-happy <triggerhappyelite@gmail.com>
2009-09-19 23:46:16 +0800

    Added more read based test cases for xml.
    They naturally fail right now since the xml layer
    hasn't been completed yet.

--- include/data_types.h
+++ include/data_types.h
@@ -3,11 +3,13 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <algorithm>
 
 using std::string;
 using std::vector;
 using std::map;
 using std::pair;
+using std::equal;
 
 struct R1Question{
 	bool operator==(const R1Question& rhs){
@@ -55,16 +57,40 @@ struct R1QData{
 typedef map<int,int> R1Answers;
 
 struct ClientConfig{
+	bool operator==(const ClientConfig& rhs){
+		if(this->ip == rhs.ip && this->port == rhs.port){
+			return true;
+		}
+		return false;
+	}
 	string ip;
 	int port;
 };
 
 struct StageData{
+	bool operator==(const StageData& rhs){
+		if(question_file == rhs.question_file
+			&& answer_file == rhs.answer_file){
+			return true;
+		}
+		return false;
+	}
 	string question_file;
 	string answer_file;
 };
 
 struct ServerConfig{
+	bool operator==(const ServerConfig& rhs){
+		if(port == rhs.port && db_path == rhs.db_path){
+			if(stage_data.size() != rhs.stage_data.size()){
+				return false;
+			}
+			if(equal(stage_data.begin() ,stage_data.end(), rhs.stage_data.begin())){
+				return true;
+			}
+		}
+		return false;
+	}
 	int port;
 	string db_path;
 	vector<StageData> stage_data;
--- src/unit_tests/xml_test.cpp
+++ src/unit_tests/xml_test.cpp
@@ -29,18 +29,48 @@ void XmlTest::q1ReadTest(){
 }
 
 void XmlTest::q1WriteTest(){
+	//TODO: Add a test case wherein the xml output is what's intended
 }
 
 void XmlTest::a1ReadTest(){
+	QFile file("resources/stage1_a.xml");
+	QString xml = file.readAll();
+	QVERIFY(xml.size() > 0);
+	R1Answers ad, td;
+	ad.insert(pair<int, int>(1,1));
+	ad.insert(pair<int, int>(2,4));
+	m_util.readR1AData(xml, td);
+	QVERIFY(td == ad);
 }
 
 void XmlTest::a1WriteTest(){
+	//TODO: Add a test case wherein the xml output is what's intended
 }
 
 void XmlTest::clientConfReadTest(){
+	QFile file("resources/client_config.xml");
+	QString xml = file.readAll();
+	QVERIFY(xml.size() > 0);
+	ClientConfig cd, td;
+	cd.ip = "192.168.1.100";
+	cd.port = 2652;
+	m_util.readClientConfig(xml, td);
+	QVERIFY(td == cd);
 }
 
 void XmlTest::serverConfReadTest(){
+	QFile file("resources/server_config.xml");
+	QString xml = file.readAll();
+	QVERIFY(xml.size() > 0);
+	ServerConfig sd, td;
+	sd.db_path = "resources/server.db";
+	sd.port = 2652;
+	StageData stemp;
+	stemp.answer_file = "resources/stage1_q.xml";
+	stemp.question_file = "resources/stage1_a.xml";
+	sd.stage_data.push_back(stemp);
+	m_util.readServerConfig(xml, td);
+	QVERIFY(td == sd);
 }
 
 QTEST_MAIN(XmlTest);
