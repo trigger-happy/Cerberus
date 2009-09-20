9d8802efb8b14aeba53bb3f54346c077843d91c1
trigger-happy <triggerhappyelite@gmail.com>
2009-09-20 10:40:14 +0800

    Added the cpp file for SqlUtil

---
+++ src/utils/sql_util.cpp
@@ -0,0 +1,19 @@
+#include "sql_util.h"
+
+bool SqlUtil::init(const QString& dbname){
+}
+
+int SqlUtil::addTeam(const QString& team_name, const QString& school){
+}
+
+int SqlUtil::addUser(const UserData& ud){
+}
+
+int SqlUtil::getScore(const QString& user_name){
+}
+
+void SqlUtil::setScore(const QString& user_name, int score){
+}
+
+bool SqlUtil::authenticate(const QString& user_name, const QString& password){
+}
