628e2595a162beb6a303ab559d28c3e6ef810ab9
trigger-happy <triggerhappyelite@gmail.com>
2009-09-21 13:03:51 +0800

    Gave SqlUtil functions a return value for testing purposes.

--- src/utils/sql_util.cpp
+++ src/utils/sql_util.cpp
@@ -1,19 +1,24 @@
 #include "sql_util.h"
 
 bool SqlUtil::init(const QString& dbname){
+	return true;
 }
 
 int SqlUtil::addTeam(const QString& team_name, const QString& school){
+	return 0;
 }
 
 int SqlUtil::addUser(const UserData& ud){
+	return 0;
 }
 
 int SqlUtil::getScore(const QString& user_name){
+	return 0;
 }
 
 void SqlUtil::setScore(const QString& user_name, int score){
 }
 
 bool SqlUtil::authenticate(const QString& user_name, const QString& password){
+	return true;
 }
