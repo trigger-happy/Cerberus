f24b8ff40fb7f8960f629121760e6988f4bbdfcf
kdblitz <kdlee1_7@yahoo.com>
2009-10-19 00:09:55 +0800

    Renamed some of the ui variables on the ui file. Started playing around with qt classes. (for round 1 only)

--- src/question_editor/question_editor.cpp
+++ src/question_editor/question_editor.cpp
@@ -15,6 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
+#include <QtGui>
 #include <QtGui/QApplication>
 #include "question_editor.h"
 #include "ui_question_editor.h"
@@ -29,11 +30,43 @@ QEditor::QEditor(QWidget *parent) : QMainWindow(parent), q_ui(new Ui::q_editor)
 	QFile f ("resources/stage1_q.xml");
 	f.open ( QIODevice::ReadOnly );
 	QString xml = f.readAll();
+	//q_ui->first_round_list
 	//cout << xml;
+	model=new QStandardItemModel(2,9);
+	/*
+	question score time ansa ansb ansc ansd boola boolb boolc boold boole
+	*/
+	for (int row=0;row<2;row++)
+	{
+		QString s="Question ";
+		s.append(QString::number(row+1));
+		QStandardItem* item=new QStandardItem(s);
+		// QStandardItem* child = new QStandardItem(QString("Item %0").arg(i) );
+		model->setItem(row,0,item);
+	}
+	q_ui->first_round_list->setModel(model);
+	connect(q_ui->first_round_list, SIGNAL(clicked(QModelIndex)),this, SLOT(on_first_list()));
+	connect(q_ui->add_q_round_1,SIGNAL(clicked()),this,SLOT(add_question_r1()));
+	
+	//connect(q_ui->toolButton,SIGNAL(clicked()),this,SLOT(on_first_list()));
+	
 }
 QEditor::~QEditor()
 {
 	delete q_ui;
+}
+
+void QEditor::on_first_list()
+{
+	q_ui->question1_text->setPlainText(q_ui->first_round_list->currentIndex().data().toString());
+	q_ui->question1_a->setText("@_@");
+	//q_ui->question1_time->;
+	q_ui->question1_score->setValue(60);
+}
+
+void QEditor::add_question_r1()
+{
+	model->appendRow(new QStandardItem("New Question"));
 }
 
 int main(int argc, char *argv[])
