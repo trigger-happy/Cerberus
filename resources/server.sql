09c2c0388c258ec471e9c3f1594391cf172d97e7
trigger-happy <triggerhappyelite@gmail.com>
2009-10-25 10:15:56 +0800

    Sql: Minor consistency fix

--- resources/server.sql
+++ resources/server.sql
@@ -1,5 +1,5 @@
 BEGIN TRANSACTION;
-CREATE TABLE "team" (team_name varchar(30) NOT NULL, school varchar(30), PRIMARY KEY(team_name));
+CREATE TABLE team(team_name varchar(30) NOT NULL, school varchar(30), PRIMARY KEY(team_name));
 /*INSERT INTO "team" VALUES('team pacquiao','notre dame');*/
 
 CREATE TABLE user(username varchar(30) NOT NULL, team_name varchar(30) NOT NULL, firstname varchar(30) NOT NULL, lastname varchar(30) NOT NULL, password varchar(30) NOT NULL, PRIMARY KEY(username), FOREIGN KEY(team_name) references team(team_name) );
