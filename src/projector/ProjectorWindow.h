f27740e1f35c45a3efa0fd14ed9094429c193a2c
Wilhansen Li <wil@nohakostudios.net>
2009-10-31 11:34:19 +0800

    Fullscreen toggle, config loading, formatting.
    F11 now toggles between full screen and normal mode.
    The configuration is "configurable" by external objects (that is,
    it doesn't lock its configuration file to "projector_config.xml").
    Also, the title displays the contest name if configured.

--- src/projector/ProjectorWindow.h
+++ src/projector/ProjectorWindow.h
@@ -1,23 +1,29 @@
-#ifndef PROJECTORWINDOW_H
-#define PROJECTORWINDOW_H
+#ifndef PROJECTOR_PROJECTORWINDOW_H_
+#define PROJECTOR_PROJECTORWINDOW_H_
 
 #include <QtGui/QMainWindow>
 
 namespace Ui {
-    class ProjectorWindow;
+	class ProjectorWindow;
 }
 
+struct ProjectorConfig;
+
 class ProjectorWindow : public QMainWindow {
-    Q_OBJECT
+	Q_OBJECT
 public:
-    ProjectorWindow(QWidget *parent = 0);
-    ~ProjectorWindow();
+	ProjectorWindow(QWidget *parent = 0);
+	~ProjectorWindow();
 
+	void loadConfigFromFile(const QString &file_path);
+	void setConfig(const ProjectorConfig &cfg);
+	const ProjectorConfig& getConfig() const;
 protected:
-    void changeEvent(QEvent *e);
-
+	void changeEvent(QEvent *e);
+	void keyReleaseEvent(QKeyEvent *event);
 private:
-    Ui::ProjectorWindow *m_ui;
+	Ui::ProjectorWindow *m_ui;
+	ProjectorConfig *m_cfg;
 };
 
-#endif // PROJECTORWINDOW_H
+#endif // PROJECTOR_PROJECTORWINDOW_H_
