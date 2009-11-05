48bec53a42f942657645db1094df60ece1545067
trigger-happy <triggerhappyelite@gmail.com>
2009-11-05 10:32:28 +0800

    Net: Added documentation for ProjectorNet.

--- include/net/projector_net.h
+++ include/net/projector_net.h
@@ -63,10 +63,14 @@ public:
 	void getContestState();
 
 	/*!
+	Get the contest time from the server. Contest time meaning
+	the time left for the current round of a contest.
 	*/
 	void getContestTime();
 
 	/*!
+	Send a notification to the server that this projector is ready
+	to accept commands.
 	*/
 	void sendReadyState();
 
@@ -106,16 +110,70 @@ signals:
 	*/
 	void onQData ( const QString& xml );
 
+	/*!
+	Emitted when a connection to the server has been established.
+	*/
 	void onConnect();
+
+	/*!
+	Emitted when the projector gets disconnected from the server.
+	*/
 	void onDisconnect();
+
+	/*!
+	Emitted when a socket error occurs.
+	\param error A QString containing the error that occurred.
+	*/
 	void onError( const QString& error );
 
+
+	/*!
+	Emitted when the state of the contest changes. This could either
+	be the round or the status or both.
+	\param round The new contest round
+	\param status The new status of the contest round.
+	*/
 	void onContestState( ushort round, CONTEST_STATUS status );
+
+	/*!
+	Emitted when the contest time changes or as a response to
+	getting the contest time.
+	\param time The time remaining for the contest round.
+	*/
 	void onContestTime( ushort time );
+
+	/*!
+	Emitted when the server sends an instruction to the projector
+	to show the time left in the contest.
+	*/
 	void onShowContestTime();
+
+	/*!
+	Emitted when the server sends an instruction to show the current
+	contest rankings.
+	\param rd An std::vector containing RankData for the current round.
+	*/
 	void onShowContestRanks( const vector<RankData>& rd );
+
+	/*!
+	Emitted when there is a change in the question state.
+	\note This gets emitted whenever any of the parameters have changed.
+	\param qnum The new question number.
+	\param time The time left for this question.
+	\param status The current status for this question.
+	*/
 	void onQuestionState( ushort qnum, ushort time, QUESTION_STATUS status );
+
+	/*!
+	Emitted when the server sends an instruction to show the answer
+	of the current question on screen.
+	*/
 	void onShowAnswer();
+
+	/*!
+	Emitted when the server sends an instruction to show the current
+	question on screen.
+	*/
 	void onShowQuestion();
 
 private:
